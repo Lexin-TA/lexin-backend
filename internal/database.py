801a20e0ec31bfac0a2d7b946e0a8bdc95adeb0c
navalgaze <ichsan.khay@gmail.com>
2025-01-05 18:32:01 +0700

    Increased pool_size to database connection.

--- internal/database.py
+++ internal/database.py
@@ -17,7 +17,7 @@ DATABASE_PORT = os.getenv('DATABASE_PORT')
 DATABASE_NAME = os.getenv('DATABASE_NAME')
 
 DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASS}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
-DB_ENGINE = create_engine(DATABASE_URL, echo=True, pool_size=64, max_overflow=128)
+DB_ENGINE = create_engine(DATABASE_URL, echo=True, pool_size=128, max_overflow=256)
 
 
 # Database Session and Table Creation.
